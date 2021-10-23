e2b01ee387978cda932ec4337e2cf5b8a1c3ba1d
Павел Кліманскі <klimanskijpavel@gmail.com>
2021-10-23 22:30:42 +0300

    описано заполнения таблиц Авторы и Книги

--- 4/Data/DbInitializer.cs
+++ 4/Data/DbInitializer.cs
@@ -2,10 +2,112 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Internal;
+using _4.Models;
 
 namespace _4.Data
 {
     public class DbInitializer
     {
+        private static char[] letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".ToCharArray();
+        private static Random random = new Random();
+
+        public static void Initialize(publishing_houseContext db)
+        {
+            db.Database.EnsureCreated();
+
+            int rowCount;
+            int rowIndex;
+
+            int minStringLength;
+            int maxStringLength;
+
+            if (!db.Authors.Any())
+            {
+                string fio;
+
+                rowCount = 700;
+                rowIndex = 0;
+                while (rowIndex < rowCount)
+                {
+                    minStringLength = 8;
+
+                    maxStringLength = 16;
+                    fio = GetString(minStringLength, maxStringLength);
+
+                    db.Authors.Add(new Author { Fio = fio });
+                    rowIndex++;
+                }
+
+                db.SaveChanges();
+            }
+
+            if (!db.Books.Any())
+            {
+                string name;
+                int total;
+                DateTime exitDate;
+                decimal basecost;
+                decimal finishcost;
+                decimal salary;
+                int authorId;
+
+                rowCount = 25000;
+                rowIndex = 0;
+
+                minStringLength = 8;
+
+                maxStringLength = 32;
+
+                name = GetString(minStringLength, maxStringLength);
+                total = random.Next(20, 500);
+                basecost = random.Next(200, 2000);
+                finishcost = random.Next(500, 5000);
+                salary = random.Next(200, 5000);
+                authorId = random.Next(1, 701);
+
+                do
+                {
+                    exitDate = GetDateTime();
+                }
+                while (exitDate.Year < random.Next(2000, 2025));
+
+                db.Books.Add(new Book
+                {
+                    Name = name,
+                    Total = total,
+                    Exitdate = exitDate,
+                    Basecost = basecost,
+                    Finishcost = finishcost,
+                    Salary = salary,
+                    AuthorId = authorId
+                });
+            }
+        }
+
+        private static string GetString(int minStringLength, int maxStringLength)
+        {
+            string result = "";
+
+            int stringLimit = minStringLength + random.Next(maxStringLength - minStringLength);
+
+            int stringPosition;
+            for (int i = 0; i < stringLimit; i++)
+            {
+                stringPosition = random.Next(letters.Length);
+
+                result += letters[stringPosition];
+            }
+
+            return result;
+        }
+
+        private static DateTime GetDateTime()
+        {
+            DateTime start = new DateTime(1995, 1, 1);
+            int range = (DateTime.Today - start).Days;
+
+            return start.AddDays(random.Next(range));
+        }
     }
 }
