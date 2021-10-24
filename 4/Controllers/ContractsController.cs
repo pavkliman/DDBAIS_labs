55a76974d80bb8143545266e1cfe9370e66f3e05
Павел Кліманскі <klimanskijpavel@gmail.com>
2021-10-24 17:32:11 +0300

    Описан ContractsController

--- 4/Controllers/ContractsController.cs
+++ 4/Controllers/ContractsController.cs
@@ -3,14 +3,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using _4.Data;
 
 namespace _4.Controllers
 {
     public class ContractsController : Controller
     {
+        private publishing_houseContext db;
+
+        public ContractsController(publishing_houseContext context)
+        {
+            db = context;
+        }
+
+        [ResponseCache(CacheProfileName = "CasheProfile")]
         public IActionResult Index()
         {
-            return View();
+            return View(db.Contracts.Include(t => t.Author).Take(20).ToList());
         }
     }
 }
