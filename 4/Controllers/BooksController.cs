1604f02cd71481dad1618aa304eff618d1374179
Павел Кліманскі <klimanskijpavel@gmail.com>
2021-10-24 17:26:33 +0300

    описан BooksController

--- 4/Controllers/BooksController.cs
+++ 4/Controllers/BooksController.cs
@@ -3,14 +3,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using _4.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace _4.Controllers
 {
     public class BooksController : Controller
     {
+        private publishing_houseContext db;
+
+        public BooksController(publishing_houseContext context)
+        {
+            db = context;
+        }
+
+        [ResponseCache(CacheProfileName = "CacheProfile")]
         public IActionResult Index()
         {
-            return View();
+            return View(db.Books.Include(s => s.Author).Take(20).ToList());
         }
     }
 }
