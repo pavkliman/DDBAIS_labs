8776b3fc6aaf20144906831ed8df5062f5c0084a
Павел Кліманскі <klimanskijpavel@gmail.com>
2021-10-24 13:28:52 +0300

    в DbInitializer описано заполнения всех таблиц

--- 4/Data/DbInitializer.cs
+++ 4/Data/DbInitializer.cs
@@ -54,34 +54,137 @@ namespace _4.Data
 
                 rowCount = 25000;
                 rowIndex = 0;
-
-                minStringLength = 8;
-
-                maxStringLength = 32;
-
-                name = GetString(minStringLength, maxStringLength);
-                total = random.Next(20, 500);
-                basecost = random.Next(200, 2000);
-                finishcost = random.Next(500, 5000);
-                salary = random.Next(200, 5000);
-                authorId = random.Next(1, 701);
-
-                do
+                while (rowIndex < rowCount)
                 {
-                    exitDate = GetDateTime();
+                    minStringLength = 8;
+
+                    maxStringLength = 32;
+                    name = GetString(minStringLength, maxStringLength);
+                    total = random.Next(20, 500);
+                    basecost = random.Next(200, 2000);
+                    finishcost = random.Next(500, 5000);
+                    salary = random.Next(200, 5000);
+                    authorId = random.Next(1, 701);
+                    do
+                    {
+                        exitDate = GetDateTime();
+                    }
+                    while (exitDate.Year < random.Next(2000, 2025));
+
+                    db.Books.Add(new Book
+                    {
+                        Name = name,
+                        Total = total,
+                        Exitdate = exitDate,
+                        Basecost = basecost,
+                        Finishcost = finishcost,
+                        Salary = salary,
+                        AuthorId = authorId
+                    });
+
+                    rowIndex++;
                 }
-                while (exitDate.Year < random.Next(2000, 2025));
+                db.SaveChanges();
+            }
 
-                db.Books.Add(new Book
+            if (!db.Contracts.Any())
+            {
+                DateTime date;
+                DateTime deadline;
+                int authorId;
+
+                rowCount = 25000;
+                rowIndex = 0;
+
+                while (rowIndex < rowCount)
                 {
-                    Name = name,
-                    Total = total,
-                    Exitdate = exitDate,
-                    Basecost = basecost,
-                    Finishcost = finishcost,
-                    Salary = salary,
-                    AuthorId = authorId
-                });
+                    do
+                    {
+                        date = GetDateTime();
+                        deadline = GetDateTime();
+                    }
+                    while (date.Year < random.Next(2000, 2025) &&
+                    deadline.Year < random.Next(2000, 2025));
+
+                    authorId = random.Next(1, 701);
+
+                    db.Contracts.Add(new Contract
+                    {
+                        Date = date,
+                        Deadline = deadline,
+                        AuthorId = authorId
+                    });
+
+                    rowIndex++;
+                }
+
+                db.SaveChanges();
+            }
+
+            if (!db.Customers.Any())
+            {
+                string customerName;
+
+                rowCount = 700;
+                rowIndex = 0;
+                while (rowIndex < rowCount)
+                {
+                    minStringLength = 8;
+
+                    maxStringLength = 16;
+                    customerName = GetString(minStringLength, maxStringLength);
+                    db.Customers.Add(new Customer { Customername = customerName });
+                    rowIndex++;
+                }
+                db.SaveChanges();
+            }
+
+            if (!db.Orders.Any())
+            {
+                string orderName;
+                DateTime startDate;
+                DateTime finishDate;
+                int exemplairs;
+                decimal totalSum;
+                int customerId;
+                int bookId;
+
+                rowCount = 20000;
+                rowIndex = 0;
+                while (rowIndex < rowCount)
+                {
+                    minStringLength = 8;
+
+                    maxStringLength = 32;
+                    orderName = GetString(minStringLength, maxStringLength);
+                    do
+                    {
+                        startDate = GetDateTime();
+                        finishDate = GetDateTime();
+                    }
+                    while (startDate.Year < random.Next(2000, 2025) && 
+                    finishDate.Year < random.Next(2000, 2025));
+
+                    exemplairs = random.Next(100, 2000);
+                    totalSum = random.Next(10000, 2000000);
+                    customerId = random.Next(1, 701);
+                    bookId = random.Next(1, 25001);
+
+                    db.Orders.Add(new Order
+                    {
+                        Ordername = orderName,
+                        Startdate = startDate,
+                        Finishdate = finishDate,
+                        Exemplairs = exemplairs,
+                        Totalsum = totalSum,
+                        CustomerId = customerId,
+                        BookId = bookId
+                    });
+
+                    rowIndex++;
+                }
+
+                db.SaveChanges();
             }
         }
 
