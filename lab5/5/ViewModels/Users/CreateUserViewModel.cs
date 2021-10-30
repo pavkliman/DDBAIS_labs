11b0dcb386f66250d96a8cfed276bba032d94223
Павел Кліманскі <klimanskijpavel@gmail.com>
2021-10-30 21:49:56 +0300

    Описаны все классы ViewModels.Users

--- lab5/5/ViewModels/Users/CreateUserViewModel.cs
+++ lab5/5/ViewModels/Users/CreateUserViewModel.cs
@@ -2,10 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace _5.ViewModels.Users
 {
     public class CreateUserViewModel
     {
+        [Display(Name = "Имя")]
+        public string UserName { get; set; }
+        [EmailAddress(ErrorMessage = "Некорректный адрес")]
+        public string Email { get; set; }
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+        [Display(Name = "Дата регистрации")]
+        [DataType(DataType.Date)]
+        public DateTime RegistrationDate { get; set; }
+
+        [Display(Name = "Роль")]
+        public string UserRole { get; set; }
+        public CreateUserViewModel()
+        {
+            UserRole = "user";
+            RegistrationDate = DateTime.Now.Date;
+        }
     }
 }
--- lab5/5/ViewModels/Users/UserViewModel.cs
+++ lab5/5/ViewModels/Users/UserViewModel.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace _5.ViewModels.Users
 {
     public class UserViewModel
     {
+        public string Id { get; set; }
+        [Display(Name = "Имя")]
+        public string UserName { get; set; }
+        [EmailAddress(ErrorMessage = "Некорректный адрес")]
+        public string Email { get; set; }
+        [Display(Name = "Дата регистрации")]
+        [DataType(DataType.Date)]
+        public DateTime RegistrationDate { get; set; }
+        [Display(Name = "Роль")]
+        public string RoleName { get; set; }
     }
 }
