aa9b249ba9a179cc23f87160b887545f2cdbb13b
Павел Кліманскі <klimanskijpavel@gmail.com>
2021-10-31 17:50:20 +0300

    Описан ContractsViewModel

--- lab5/5/ViewModels/ContractsViewModel.cs
+++ lab5/5/ViewModels/ContractsViewModel.cs
@@ -4,11 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using _5.Models;
+using _5.Models;
 
 namespace _5.ViewModels
 {
     public class ContractsViewModel
     {
-        
+        public IEnumerable<Contract> Contracts { get; set; }
+        public FilterContractViewModel FilterContractViewModel { get; set; }
+        public PageViewModel PageViewModel { get; set; }
+        public SortViewModel SortViewModel { get; set; }
     }
 }
