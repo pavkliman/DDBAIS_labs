7d1deb60e6f034c34d92b87e42c01646628ad8eb
Павел Кліманскі <klimanskijpavel@gmail.com>
2021-10-31 17:52:33 +0300

    Описан OrdersViewModel

--- lab5/5/ViewModels/OrdersViewModel.cs
+++ lab5/5/ViewModels/OrdersViewModel.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using _5.Models;
 
 namespace _5.ViewModels
 {
     public class OrdersViewModel
     {
+        public IEnumerable<Order> Orders { get; set; }
+        public FilterOrderViewModel FilterOrderViewModel { get; set; }
+        public PageViewModel PageViewModel { get; set; }
+        public SortViewModel SortViewModel { get; set; }
     }
 }
