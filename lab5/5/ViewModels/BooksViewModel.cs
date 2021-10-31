df8537802a37e4d9acbf5722eb3144dc478ef840
Павел Кліманскі <klimanskijpavel@gmail.com>
2021-10-31 17:43:41 +0300

    Описан BooksViewModel

--- lab5/5/ViewModels/BooksViewModel.cs
+++ lab5/5/ViewModels/BooksViewModel.cs
@@ -9,6 +9,9 @@ namespace _5.ViewModels
 {
     public class BooksViewModel
     {
-       
+       public IEnumerable<Book> Books { get; set; }
+       public FilterBookViewModel FilterBookViewModel { get; set; }
+       public PageViewModel PageViewModel { get; set; }
+       public SortViewModel SortViewModel { get; set; }
     }
 }
