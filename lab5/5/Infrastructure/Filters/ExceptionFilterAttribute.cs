132e7ed0eaa65b02fae5a9cf4c4bce9032c2b6c7
Павел Кліманскі <klimanskijpavel@gmail.com>
2021-10-31 21:56:54 +0300

    Описан ExceptionFilterAttribute

--- lab5/5/Infrastructure/Filters/ExceptionFilterAttribute.cs
+++ lab5/5/Infrastructure/Filters/ExceptionFilterAttribute.cs
@@ -2,10 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace _5.Infrastructure.Filters
 {
-    public class ExceptionFilterAttribute
+    public class ExceptionFilterAttribute : Attribute, IExceptionFilter
     {
+        public void OnException(ExceptionContext context)
+        {
+            string actionName = context.ActionDescriptor.DisplayName; //имя метода действия
+            string exceptionStack = context.Exception.StackTrace;
+            string exceptionMessage = context.Exception.Message;
+            context.Result = new ContentResult
+            {
+                Content = $"В методе {actionName} возникло исключение: \n {exceptionMessage} \n {exceptionStack}"
+            };
+            context.ExceptionHandled = true;
+        }
     }
 }
