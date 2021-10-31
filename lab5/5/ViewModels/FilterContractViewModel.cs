ed0fda6aadb80079385a0efa1dd7adee72e27dc5
Павел Кліманскі <klimanskijpavel@gmail.com>
2021-10-31 15:46:58 +0300

    описан FilterContractViewModel

--- lab5/5/ViewModels/FilterContractViewModel.cs
+++ lab5/5/ViewModels/FilterContractViewModel.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace _5.ViewModels
 {
     public class FilterContractViewModel
     {
+        public int ContractId { get; set; }
+        [Display(Name = "Дата заключения")]
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+        [Display(Name = "Дата расторжения")]
+        [DataType(DataType.Date)]
+        public DateTime DeadLine { get; set; }
+        [Display(Name = "Имя автора")]
+        public string AuthorName { get; set; }
     }
 }
