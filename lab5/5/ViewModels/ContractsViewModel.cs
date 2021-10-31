37f480dd1a3227d42eb2624a83020d36e4d2404f
Павел Кліманскі <klimanskijpavel@gmail.com>
2021-10-31 17:59:52 +0300

    описан FilterBookViewModel

--- lab5/5/ViewModels/ContractsViewModel.cs
+++ lab5/5/ViewModels/ContractsViewModel.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using _5.Models;
-using _5.Models;
 
 namespace _5.ViewModels
 {
--- lab5/5/ViewModels/FilterBookViewModel.cs
+++ lab5/5/ViewModels/FilterBookViewModel.cs
@@ -2,10 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace _5.ViewModels
 {
     public class FilterBookViewModel
     {
+        public int BookId { get; set; }
+        [Display(Name = "Название книги")]
+        public string BookName { get; set; }
+        [Display(Name = "Тираж")]
+        public int Total { get; set; }
+        [Display(Name = "Дата выхода из печати")]
+        [DataType(DataType.Date)]
+        public DateTime ExitDate { get; set; }
+        [Display(Name = "Себестоимость")]
+        public decimal BaseCost { get; set; }
+        [Display(Name = "Розничная цена")]
+        public decimal FinishCost { get; set; }
+        [Display(Name = "Гонорар автора")]
+        public decimal Salary { get; set; }
+        [Display(Name = "имя автора")]
+        public string AuthorName { get; set; }
     }
 }
