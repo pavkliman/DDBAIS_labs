22098a3813f6586fff67905648e18fcc8cc1716f
Павел Кліманскі <klimanskijpavel@gmail.com>
2021-10-31 16:11:50 +0300

    Опиман CustomerViewModel

--- lab5/5/ViewModels/CustomersViewModel.cs
+++ lab5/5/ViewModels/CustomersViewModel.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using _5.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace _5.ViewModels
 {
     public class CustomersViewModel
     {
+        public IEnumerable<Customer> Customers { get; set; }
+        [Display(Name = "Имя заказчика")]
+        public string CustomerName { get; set; }
+
+        public PageViewModel PageViewModel { get; set; }
+        // Порядок сортировки
+        public SortViewModel SortViewModel { get; set; }
     }
 }
