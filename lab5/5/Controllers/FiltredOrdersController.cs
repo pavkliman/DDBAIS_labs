a05e85c98ed7732adaa4a2157c56c11fdc9ad051
Павел Кліманскі <klimanskijpavel@gmail.com>
2021-11-01 08:54:12 +0300

    Описан Infrastructure.Transformations

--- lab5/5/Controllers/FiltredOrdersController.cs
+++ lab5/5/Controllers/FiltredOrdersController.cs
@@ -3,13 +3,39 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using _5.Data;
+using _5.Models;
+using _5.Infrastructure;
+using _5.Infrastructure.Filters;
+using _5.ViewModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace _5.Controllers
 {
     public class FiltredOrdersController : Controller
     {
-        public IActionResult Index()
+
+        private readonly PublishingLabContext _context;
+        private FilterOrderViewModel _operation = new FilterOrderViewModel
         {
+            CustomerName = "",
+            BookName = ""
+        };
+
+        public FiltredOrdersController(PublishingLabContext context)
+        {
+            _context = context;
+        }
+
+        [SetToSession("SortState")]
+        public IActionResult Index(SortState sortOrder)
+        {
+            var sessionOperation = HttpContext.Session.Get("Operation");
+            var sessionSortState = HttpContext.Session.Get("SortState");
+            if (sessionOperation != null)
+                _operation = Transformations.DictionaryToObject<FilterOrderViewModel>(sessionOperation);
+            if ((sessionSortState != null))
+                if ((sessionSortState.Count > 0) & (sortOrder == SortState.No)) sortOrder = (SortState)Enum.Parse(typeof(SortState), sessionSortState["sortOrder"]);
             return View();
         }
     }
--- lab5/5/Infrastructure/Transformations.cs
+++ lab5/5/Infrastructure/Transformations.cs
@@ -2,10 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Reflection;
 
 namespace _5.Infrastructure
 {
-    public class Transformations
+    public static class Transformations
     {
+        public static T DictionaryToObject<T>(IDictionary<string, string> dict) where T : new()
+        {
+            var t = new T();
+            PropertyInfo[] properties = t.GetType().GetProperties();
+
+            foreach (PropertyInfo property in properties)
+            {
+                if (!dict.Any(x => x.Key.Equals(property.Name, StringComparison.InvariantCultureIgnoreCase)))
+                    continue;
+
+                KeyValuePair<string, string> item = dict.First(x => x.Key.Equals(property.Name, StringComparison.InvariantCultureIgnoreCase));
+
+                Type tPropertyType = t.GetType().GetProperty(property.Name).PropertyType;
+
+                Type newT = Nullable.GetUnderlyingType(tPropertyType) ?? tPropertyType;
+
+                object newA = Convert.ChangeType(item.Value, newT);
+                t.GetType().GetProperty(property.Name).SetValue(t, newA, null);
+            }
+            return t;
+        }
     }
 }
