56415864918abd91d90426416f8c9bec9fa7cc94
Павел Кліманскі <klimanskijpavel@gmail.com>
2021-11-07 15:16:20 +0300

    Описаны представления Acount.Manage

--- lab5/5/Areas/Identity/Pages/Account/Manage/PersonalData.cshtml.cs
+++ lab5/5/Areas/Identity/Pages/Account/Manage/PersonalData.cshtml.cs
@@ -1,16 +1,36 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using _5.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
 
 namespace _5.Areas.Identity.Pages.Account.Manage
 {
+    [Authorize]
     public class PersonalDataModel : PageModel
     {
-        public void OnGet()
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ILogger<PersonalDataModel> _logger;
+
+        public PersonalDataModel(
+            UserManager<ApplicationUser> userManager,
+            ILogger<PersonalDataModel> logger)
         {
+            _userManager = userManager;
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> OnGet()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Ќевозможно загрузить пользовател€ с ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            return Page();
         }
     }
 }
