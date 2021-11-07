20b787841500b61bbffc8489625e34f3f43c97dd
Павел Кліманскі <klimanskijpavel@gmail.com>
2021-11-07 14:48:08 +0300

    Описан  IdentityHostingSturtup

--- lab5/5/Areas/Identity/IdentityHostingStartup.cs
+++ lab5/5/Areas/Identity/IdentityHostingStartup.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 
+[assembly: HostingStartup(typeof(_5.Areas.Identity.IdentityHostingStartup))]
 namespace _5.Areas.Identity
 {
     public class IdentityHostingStartup
